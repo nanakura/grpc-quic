3afb4f8c4fd888d51c50078c883e56c91a4ef4f8
Asphodelus <1749094641@qq.com>
2024-04-15 19:03:05 +0800

    init

---
+++ example/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"github.com/quic-go/quic-go"
+	"google.golang.org/grpc"
+	"log"
+	"math/big"
+	"quic-grpc"
+	hello_grpc "quic-grpc/proto"
+	"time"
+)
+
+type hello struct {
+	hello_grpc.UnimplementedHelloGRPCServer
+}
+
+func (s *hello) SayHi(ctx context.Context, req *hello_grpc.Req) (res *hello_grpc.Res, err error) {
+	fmt.Println(req.GetMessage())
+	return &hello_grpc.Res{Message: "from server"}, nil
+}
+
+// Setup a bare-bones TLS config for the server
+func generateTLSConfig() (*tls.Config, error) {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		return nil, err
+	}
+
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		return nil, err
+	}
+
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{Certificates: []tls.Certificate{tlsCert}}, nil
+}
+
+func main() {
+	var (
+		client *grpc.ClientConn
+		server *grpc.Server
+
+		err error
+	)
+	tlsConf, err := generateTLSConfig()
+	if err != nil {
+		panic(err)
+	}
+	addr := "localhost:1234"
+	qln, err := quic.ListenAddr(addr, tlsConf, nil)
+	if err != nil {
+		panic(err)
+	}
+	ln := quic_grpc.Listen(qln)
+	server = grpc.NewServer()
+	hello_grpc.RegisterHelloGRPCServer(server, &hello{})
+	go func() {
+		if err := server.Serve(ln); err != nil {
+			panic(err)
+		}
+	}()
+
+	// client
+	cliTlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	creds := quic_grpc.NewCredentials(cliTlsConf)
+
+	dialer := quic_grpc.NewQuicDialer(cliTlsConf)
+	grpcOpts := []grpc.DialOption{
+		grpc.WithContextDialer(dialer),
+		grpc.WithTransportCredentials(creds),
+	}
+
+	client, err = grpc.Dial(addr, grpcOpts...)
+	if err != nil {
+		panic(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	helloGRPCClient := hello_grpc.NewHelloGRPCClient(client)
+	resp, err := helloGRPCClient.SayHi(ctx, &hello_grpc.Req{
+		Message: "Tom",
+	})
+	if err != nil {
+		panic(err)
+	}
+	log.Println(resp.GetMessage())
+}
---
+++ go.mod
@@ -0,0 +1,25 @@
+module quic-grpc
+
+go 1.22
+
+require (
+	github.com/quic-go/quic-go v0.42.0
+	google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf v1.33.0
+)
+
+require (
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.5 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	go.uber.org/mock v0.4.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.9.1 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
+)
